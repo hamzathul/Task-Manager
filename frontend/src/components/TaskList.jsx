0e09b96337044d9f4a32fba61bc93b033933eb1e
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-11-25 08:52:28 +0530

    UI Changes

--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -12,15 +12,35 @@ const TaskList = ({ title, tasks, onUpdate, onDelete }) => {
           <div>
             <h3 className="font-bold">{task.title}</h3>
             <p className="text-sm">{task.description}</p>
-            <p className="text-sm text-gray-400">Due: {task.dueDate}</p>
+            <p
+              className={`text-sm ${
+                new Date(task.dueDate) < new Date()
+                  ? "text-red-500"
+                  : "text-gray-400"
+              }`}
+            >
+              Due: {new Date(task.dueDate).toLocaleDateString("en-US")}
+            </p>
           </div>
           <div>
-            {task.status !== "In Progress" && <button
-              onClick={() => onUpdate(task._id, { status: "In Progress" })}
-              className="bg-blue-600 px-4 py-2 rounded-lg text-sm mr-2"
-            >
-              Move to In Progress
-            </button>}
+            {task.status !== "In Progress" && (
+              <button
+                onClick={() => onUpdate(task._id, { status: "In Progress" })}
+                className="bg-blue-600 px-4 py-2 rounded-lg text-sm mr-2"
+              >
+                Move to In Progress
+              </button>
+            )}
+
+            {task.status !== "Completed" && (
+              <button
+                onClick={() => onUpdate(task._id, { status: "Completed" })}
+                className="bg-green-600 px-4 py-2 rounded-lg text-sm mr-2"
+              >
+                Completed
+              </button>
+            )}
+
             <button
               onClick={() => onDelete(task._id)}
               className="bg-red-600 px-4 py-2 rounded-lg text-sm"
